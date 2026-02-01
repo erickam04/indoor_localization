c742075316eeae0d960931aa06c8aa9d2e84393d
erickam04 <84125054+erickam04@users.noreply.github.com>
2026-02-02 02:24:32 +0900

    Added wall attenuation in RSSI calculation
    Added wall handling to IndoorNavigationEnv for RSSI calculation.

--- BLE_indoor_localization/sim/env.py
+++ BLE_indoor_localization/sim/env.py
@@ -1,7 +1,8 @@
 import numpy as np
+from sim.obstacles import Wall
 
 class IndoorNavigationEnv:
-    def __init__(self, config, beacons_pos):
+    def __init__(self, config, beacons_pos, walls_config):
         
         #When defining parameters, use ENV_CONFIG from config.py
         #For beacons, use BEACONS
@@ -11,13 +12,22 @@ class IndoorNavigationEnv:
         self.n = config["n"]
         self.sigma = config["sigma"]   
         self.beacons = []
+        self.walls = []
 
         for b in beacons_pos:
             self.add_beacon(b[0], b[1])
 
+        for w in walls_config:
+            self.add_wall(w[0], w[1], w[2], w[3])
+
 
     def add_beacon(self, x, y):
         self.beacons.append([x, y])
+
+    def add_wall(self, x1, y1, x2, y2):
+        wall = Wall(x1, y1, x2, y2)
+        self.walls.append(wall)
+
 
     def get_beacons(self):
         return np.array(self.beacons)
@@ -37,7 +47,19 @@ class IndoorNavigationEnv:
         # RSSI = P0 - 10 * n * log10(d)
         rssi = self.p0 - 10 * self.n * np.log10(distances)
 
-        # 3. Add Gaussian noise
+        #Calculate wall attenuation
+
+        for i, beacon in enumerate(beacons):
+            total_attenuation = 0.0
+        
+            for wall in self.walls:
+                if wall.is_intersect(beacon, true_pos):
+                    total_attenuation += wall.attenuation
+                
+        
+            rssi[i] -= total_attenuation
+
+        # Add Gaussian noise
         if noise:
             noise_val = np.random.normal(0, self.sigma, size=len(rssi))
             rssi += noise_val
